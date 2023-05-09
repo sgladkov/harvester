00c1a0b0bd3b86c2d41c2f15100f74a2ad35aa25
sgladkov <one.more.cpp.programmer@gmail.com>
2023-05-09 16:04:23 +0300

    move agent config to separate package

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"flag"
+	config2 "github.com/sgladkov/harvester/internal/config"
 	"github.com/sgladkov/harvester/internal/connection"
 	"github.com/sgladkov/harvester/internal/logger"
 	"github.com/sgladkov/harvester/internal/reporter"
 	"go.uber.org/zap"
 	"log"
-	"os"
-	"strconv"
-	"strings"
 	"time"
 )
 
@@ -18,42 +15,16 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	endpoint := flag.String("a", "localhost:8080", "endpoint to start server (localhost:8080 by default)")
-	pollInterval := flag.Int("p", 2, "poll interval")
-	reportInterval := flag.Int("r", 10, "report interval")
-	flag.Parse()
-	// check environment
-	address := os.Getenv("ADDRESS")
-	if len(address) > 0 {
-		*endpoint = address
-	}
-	reportStr := os.Getenv("REPORT_INTERVAL")
-	if len(reportStr) > 0 {
-		val, err := strconv.ParseInt(reportStr, 10, 32)
-		if err != nil {
-			logger.Log.Fatal("Failed to interpret REPORT_INTERVAL environment variable",
-				zap.String("value", reportStr), zap.Error(err))
-		}
-		*reportInterval = int(val)
-	}
-	pollStr := os.Getenv("POLL_INTERVAL")
-	if len(pollStr) > 0 {
-		val, err := strconv.ParseInt(pollStr, 10, 32)
-		if err != nil {
-			logger.Log.Fatal("Failed to interpret POLL_INTERVAL environment variable",
-				zap.String("value", pollStr), zap.Error(err))
-		}
-		*pollInterval = int(val)
+
+	config := config2.AgentConfig{}
+	err = config.Read()
+	if err != nil {
+		logger.Log.Fatal("failed to read config params", zap.Error(err))
 	}
 
-	// add default url scheme if required
-	if !strings.HasPrefix(*endpoint, "http://") && !strings.HasPrefix(*endpoint, "https://") {
-		*endpoint = "http://" + *endpoint
-	}
-
-	r := connection.NewRestyClient(*endpoint)
+	r := connection.NewRestyClient(*config.Endpoint)
 	m := reporter.NewReporter(r)
-	pollTicker := time.NewTicker(time.Duration(*pollInterval) * time.Second)
+	pollTicker := time.NewTicker(time.Duration(*config.PollInterval) * time.Second)
 	defer pollTicker.Stop()
 	go func() {
 		for range pollTicker.C {
@@ -64,7 +35,7 @@ func main() {
 			logger.Log.Info("Metrics are read")
 		}
 	}()
-	reportTicker := time.NewTicker(time.Duration(*reportInterval) * time.Second)
+	reportTicker := time.NewTicker(time.Duration(*config.ReportInterval) * time.Second)
 	defer reportTicker.Stop()
 	go func() {
 		for range reportTicker.C {
